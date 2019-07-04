ec2d9a50217d6238886f60c5fb634ae9418b7e1d
kevin lang <langkevina@gmail.com>
2019-07-04 12:59:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,8 +7,8 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @name = student_hash("name")
-    @location = student_hash("location")
+    @name = student_hash["name"]
+    @location = student_hash["location"]
     @@all << self
   end
 
