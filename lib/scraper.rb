76282523292687eaccf27a24f94df2511414824d
kevin lang <langkevina@gmail.com>
2019-07-04 14:38:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,7 +28,7 @@ class Scraper
          vital.css(".social-icon-container").children.each do |child|
           #  case child.attributes["href"].value
           binding.pry
-           if child.attributes["href"].value != nil
+           if child.attributes["href"] != nil
              if child.attributes["href"].value.include?("twitter")
                returnHash[:twitter] = child.attributes["href"].value
              elsif child.attributes["href"].value.include?("linkedin")
