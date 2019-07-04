c39ffc001c01c22c628b5d1c7b8fef3508f7c5bc
kevin lang <langkevina@gmail.com>
2019-07-04 13:40:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
-    cards = doc.css(".student-card")
+    cards = doc.css(".student-card").css(".card-text-container")
     returnArr = []
     cards.each do |team|
       binding.pry
