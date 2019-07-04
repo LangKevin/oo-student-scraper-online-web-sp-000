9284a7eebdb97f476e820856e75c08d7af9e9683
kevin lang <langkevina@gmail.com>
2019-07-04 13:22:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,13 +9,24 @@ class Scraper
     returnArr = []
     cards.each do |item|
       binding.pry
-      returnArr << {:name => item}  
-    end    
+      returnArr << {:name => item}
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc := Nokogiri::HTML(url)
+       cards = doc.css(".student-card")
+       returnHash = {}
+       cards.each do |item|
+         binding.pry
+         returnHash[:twitter] =
+         returnHash[:linkedin] =
+         returnHash[:github] =
+         returnHash[:blog] =
+         returnHash[:profile_quote] =
+         returnHash[:bio] =
+       end
+       returnHash    
   end
 
 end
-
