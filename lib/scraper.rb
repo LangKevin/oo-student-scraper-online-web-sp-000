4d7d34e9a9db08dcbea79e67fb84cc78514fd1fd
kevin lang <langkevina@gmail.com>
2019-07-04 13:08:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,13 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc := Nokogiri::HTML(index_url)
+    cards = doc.css(".student-card")
+    returnArr = []
+    cards.each do |item|
+      binding.pry
+      returnArr << {:name => item}  
+    end    
   end
 
   def self.scrape_profile_page(profile_url)
