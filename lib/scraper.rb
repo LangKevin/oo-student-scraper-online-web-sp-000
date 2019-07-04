6d300f7e1538e23efad87fc677a0126e43b634e6
kevin lang <langkevina@gmail.com>
2019-07-04 13:34:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,8 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-   html = open(index_url)
-    doc = Nokogiri::HTML(html)
+  #  html = open(index_url)
+    doc = Nokogiri::HTML(index_url)
     cards = doc.css(".student-card")
     returnArr = []
 binding.pry
