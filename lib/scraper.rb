de71ef6410883c7f75f72893434d6273bad9f96b
kevin lang <langkevina@gmail.com>
2019-07-04 14:54:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,7 @@ class Scraper
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
 
-       vitals = doc.css(".main-wrapper profile")
+       vitals = doc.css(".main-wrapper")
  binding.pry
        returnHash = {}
        vitals.each do |vital|
